06752bbcba6f5075c8045fa861276ed19366c273
satreix <satreix@gmail.com>
2022-04-23 00:30:00 +0100

    cc: Add gflags

--- src/cpp/hello_world/hello_world.cc
+++ src/cpp/hello_world/hello_world.cc
@@ -1,56 +1,15 @@
-#include <getopt.h>
 #include <iostream>
+
+#include "gflags/gflags.h"
+
 #include "src/cpp/hello_world/greet/greet.h"
+
+DEFINE_string(name, "World", "name to greet");
 
 int
 main(int argc, char **argv)
 {
-  int c;
-  std::string name = "World";
-
-  static struct option long_options[] = {
-    { "name", required_argument,                 0, 'n'                 },
-    {      0,                 0,                 0,                 0   },
-  };
-
-  while (1)
-  {
-    int option_index = 0;
-    c = getopt_long(argc, argv, ":d:", long_options, &option_index);
-
-    if (c == -1)
-    {
-      break;
-    }
-
-    switch (c)
-    {
-      case 0:
-
-        if (long_options[option_index].flag != 0)
-        {
-          break;
-        }
-        printf("option %s", long_options[option_index].name);
-
-        if (optarg)
-        {
-          printf(" with arg %s", optarg);
-        }
-        printf("\n");
-        break;
-
-      case 'n':
-        name = optarg;
-        break;
-
-      case '?':
-        break;
-
-      default:
-        abort();
-    }
-  }
-
-  std::cout << greet::greet(name) << std::endl;
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  std::cout << greet::greet(FLAGS_name) << std::endl;
+  return 0;
 }
