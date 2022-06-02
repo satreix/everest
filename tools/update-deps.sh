e6e290a2834104f1d4867213c82c820811b6a035
satreix <satreix@gmail.com>
2022-06-02 22:10:52 +0100

    Update

--- tools/update-deps.sh
+++ tools/update-deps.sh
@@ -10,8 +10,10 @@ touch src/proto/helloworld/empty.go
 go mod tidy
 rm src/proto/helloworld/empty.go
 GO_DEPS_FILE="third_party/go/deps.bzl"
-echo -e "def go_dependencies():\n    pass" >"$GO_DEPS_FILE"
 bazel run //:gazelle -- update-repos -from_file=go.mod -prune -to_macro "${GO_DEPS_FILE}%go_dependencies"
+sed '/^$/d' "$GO_DEPS_FILE" >"${GO_DEPS_FILE}.new"
+mv "${GO_DEPS_FILE}.new" "$GO_DEPS_FILE"
+bazel run //:buildifier
 
 section "Java"
 echo >&2 "Manually update maven_install in WORKSPACE"
