d4cc59330e2a079499c41b94280c24be68ef7854
satreix <satreix@gmail.com>
2022-10-29 16:20:14 +0100

    update

--- tools/update-deps.sh
+++ tools/update-deps.sh
@@ -2,28 +2,27 @@
 set -euo pipefail
 
 section() {
-    echo >&2 "===> $1"
+    echo "===> $1" >&2
 }
 
-# Run Java first for pinning reason.
-section "Java"
-echo >&2 "Manually update maven_install in WORKSPACE"
-REPIN=1 bazel run @unpinned_maven//:pin
+export CARGO_BAZEL_REPIN=1
+export REPIN=1
 
 section "Go"
-touch src/proto/helloworld/empty.go
-go mod tidy
-rm src/proto/helloworld/empty.go
+bazel run @go_sdk//:bin/go -- mod tidy -e
 GO_DEPS_FILE="third_party/go/deps.bzl"
 bazel run //:gazelle -- update-repos -from_file=go.mod -prune -to_macro "${GO_DEPS_FILE}%go_dependencies"
 sed '/^$/d' "$GO_DEPS_FILE" >"${GO_DEPS_FILE}.new"
 mv "${GO_DEPS_FILE}.new" "$GO_DEPS_FILE"
 bazel run //:buildifier
 
+section "Java"
+bazel run @unpinned_maven//:pin
+
 section "Python"
 ./tools/python/update-deps.sh
 
 section "Rust"
-CARGO_BAZEL_REPIN=1 bazel sync --only=crate_index
+bazel sync --only=crate_index
 
 ./tools/format.sh
