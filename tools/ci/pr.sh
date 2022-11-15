8357a8ac503c35a54f3e15b642b0180b5c182649
satreix <satreix@gmail.com>
2022-11-15 22:20:53 +0000

    use gh action PR base sha

--- tools/ci/pr.sh
+++ tools/ci/pr.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -eufo pipefail
 
+BASE_REF=${BASE_REF:-main}
 CI=${CI:-false}
 if [ "$CI" = true ] ; then
     BAZEL="./bin/bazel"
@@ -10,6 +11,6 @@ else
     BAZEL_CMD_ARGS=""
 fi
 
-$BAZEL build $BAZEL_CMD_ARGS //src/go/ci/driver
+$BAZEL build $BAZEL_CMD_ARGS --remote_download_outputs=toplevel //src/go/ci/driver
 DRIVER="$($BAZEL cquery $BAZEL_CMD_ARGS --output=files //src/go/ci/driver)"
-"$DRIVER" -bazel "$BAZEL" main
+"$DRIVER" -bazel "$BAZEL" "$BASE_REF"
