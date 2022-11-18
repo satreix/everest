8bd38d5ec61cfed046f936e1370faba202ee2448
satreix <satreix@gmail.com>
2022-11-18 23:03:40 +0000

    feat: Add Objective-C example

---
+++ src/objc/hello_world/main.m
@@ -0,0 +1,9 @@
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        NSLog(@"log message");
+        printf("Hello, World!\n");
+    }
+    return 0;
+}
