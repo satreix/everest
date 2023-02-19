ad899cdfe351d3813fc75e2618426dba91949f52
satreix <satreix@gmail.com>
2023-02-19 17:33:21 +0000

    add-cel-python-test

---
+++ src/python/cel/cel_test.py
@@ -0,0 +1,23 @@
+import unittest
+
+import celpy
+
+
+class CelTest(unittest.TestCase):
+    def test_cel_101(self):
+        env = celpy.Environment()
+        ast = env.compile("""
+account.balance >= transaction.withdrawal
+    || (account.overdraftProtection
+    && account.overdraftLimit >= transaction.withdrawal - account.balance)
+""")
+        program = env.program(ast)
+        result = program.evaluate({
+            "account": celpy.json_to_cel({"balance": 500, "overdraftProtection": False}),
+            "transaction": celpy.json_to_cel({"withdrawal": 600}),
+        })
+        self.assertEqual(result, celpy.celtypes.BoolType(False))
+
+
+if __name__ == "__main__":
+    unittest.main()
