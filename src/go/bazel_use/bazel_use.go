5a7f88f9da7c94927290f2ae91053048684fadf7
satreix <satreix@gmail.com>
2023-02-26 19:59:28 +0000

    add tests

--- src/go/bazel_use/bazel_use.go
+++ src/go/bazel_use/bazel_use.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"crypto/sha256"
+	"errors"
 	"flag"
 	"fmt"
 	"io"
@@ -21,59 +22,36 @@ var workspaceRE = regexp.MustCompile(`(?m)workspace\(\s*name = "(.+)",?\s*\)`)
 
 func main() {
 	flag.Usage = func() {
-		fmt.Fprintln(flag.CommandLine.Output(), "usage: bazel_use URI [REVISION]")
+		fmt.Fprintln(flag.CommandLine.Output(), "usage: bazel_use URI")
 		flag.PrintDefaults()
 	}
 	flag.Parse()
 
-	if flag.NArg() < 1 {
+	if flag.NArg() != 1 {
 		fmt.Fprintln(flag.CommandLine.Output(), "error: missing URI")
 		flag.Usage()
 		os.Exit(1)
 	}
 
-	uri, err := url.Parse(flag.Arg(0))
+	repo, err := parseRepository(flag.Arg(0))
 	if err != nil {
-		parts := strings.Split(flag.Arg(0), "/")
-		log.Printf("parts=%#v", parts)
-
-		var path string
-		if len(parts) == 1 {
-			path = fmt.Sprintf("/bazelbuild/%s", parts[0])
-		} else if len(parts) == 2 {
-			path = fmt.Sprintf("/%s/%s", parts[0], parts[1])
-		} else {
-			fmt.Fprintln(flag.CommandLine.Output(), "error: invalid URI %s", flag.Arg(0))
-			flag.Usage()
-			os.Exit(1)
-		}
-
-		uri = &url.URL{
-			Scheme: "https",
-			Host:   "github.com",
-			Path:   path,
-		}
+		fmt.Fprintf(flag.CommandLine.Output(), "error: invalid URI %s", flag.Arg(0))
+		flag.Usage()
+		os.Exit(1)
 	}
-
-	if uri.Host != "github.com" {
-		log.Fatal("only github.com is supported (PR are welcome)")
-	}
-
-	pathParts := strings.Split(uri.Path, "/")
-	owner := pathParts[1]
-	repo := pathParts[2]
 
 	gh := github.NewClient(nil)
 
 	ctx := context.Background()
-	releases, _, err := gh.Repositories.ListReleases(ctx, owner, repo, &github.ListOptions{})
+
+	releases, _, err := gh.Repositories.ListReleases(ctx, repo.Org, repo.Name, &github.ListOptions{})
 	if err != nil {
 		log.Fatalf("github error: %s", err)
 	}
 
 	for _, release := range releases {
 		var name string
-		if cr, _, err := gh.Repositories.DownloadContents(ctx, owner, repo, "WORKSPACE", &github.RepositoryContentGetOptions{Ref: release.GetTagName()}); err == nil {
+		if cr, _, err := gh.Repositories.DownloadContents(ctx, repo.Org, repo.Name, "WORKSPACE", &github.RepositoryContentGetOptions{Ref: release.GetTagName()}); err == nil {
 			workspaceFile, err := io.ReadAll(cr)
 			if err != nil {
 				log.Fatal("workspace file read error")
@@ -81,10 +59,10 @@ func main() {
 			name = parseWorkspaceName(string(workspaceFile))
 		}
 		if name == "" {
-			name = fmt.Sprintf("com_github_%s_%s", owner, repo)
+			name = fmt.Sprintf("com_github_%s_%s", repo.Org, repo.Name)
 		}
 
-		downloadURL := fmt.Sprintf("https://github.com/bazelbuild/rules_cc/archive/%s.tar.gz", release.GetTagName())
+		downloadURL := fmt.Sprintf("https://%s/%s/%s/archive/%s.tar.gz", repo.Host, repo.Org, repo.Name, release.GetTagName())
 		stripPrefix := fmt.Sprintf("%s-%s", repo, strings.TrimPrefix(release.GetTagName(), "v"))
 
 		sha256, err := getSHA256(downloadURL)
@@ -142,4 +120,52 @@ func getSHA256(url string) (string, error) {
 
 	hash := sha256.Sum256(body)
 	return fmt.Sprintf("%x", hash), nil
+}
+
+type repository struct {
+	Host string
+	Org  string
+	Name string
+}
+
+func parseRepository(s string) (*repository, error) {
+	uri, err := url.Parse(s)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Printf("uri: %#v", uri)
+
+	if uri.Host == "" {
+		parts := strings.Split(s, "/")
+		fmt.Fprintf(os.Stderr, "parts=%#v\n", parts)
+
+		var path string
+		if len(parts) == 1 {
+			path = fmt.Sprintf("/bazelbuild/%s", parts[0])
+		} else if len(parts) == 2 {
+			path = fmt.Sprintf("/%s/%s", parts[0], parts[1])
+		} else {
+			return nil, fmt.Errorf("error: invalid URI: %s", s)
+		}
+
+		uri = &url.URL{
+			Scheme: "https",
+			Host:   "github.com",
+			Path:   path,
+		}
+	}
+
+	if uri.Host != "github.com" {
+		return nil, errors.New("only github.com is supported (PR are welcome)")
+	}
+
+	pathParts := strings.Split(uri.Path, "/")
+	repo := repository{
+		Host: uri.Host,
+		Org:  pathParts[1],
+		Name: pathParts[2],
+	}
+
+	return &repo, nil
 }
