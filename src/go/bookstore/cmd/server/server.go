557e4bb7828d396dabd9742ffe68813e8b47b76c
satreix <satreix@gmail.com>
2023-04-01 20:28:30 +0100

    cleanup

--- src/go/bookstore/cmd/server/server.go
+++ src/go/bookstore/cmd/server/server.go
@@ -61,14 +61,6 @@ type bookstoreService struct {
 func newBookstoreService(logger zerolog.Logger) *bookstoreService {
 	return &bookstoreService{
 		logger: logger,
-		data: []*pb.Book{
-			{Title: "title1", Author: "author1", PageCount: 10},
-			{Title: "title2", Author: "author2", PageCount: 20},
-			{Title: "title3", Author: "author3", PageCount: 30},
-			{Title: "title4", Author: "author4", PageCount: 40},
-			{Title: "title5", Author: "author5", PageCount: 50},
-			{Title: "title6", Author: "author6", PageCount: 60},
-		},
 	}
 }
 
@@ -94,18 +86,12 @@ func (s *bookstoreService) ListBooks(ctx context.Context, in *pb.ListBooksReques
 			return nil, status.Errorf(codes.Internal, "compile error: %s", err)
 		}
 
-		s.logger.Info().Msg("ast compiled ok")
-
 		checkedExpr, err := cel.AstToCheckedExpr(ast)
 		if err != nil {
 			return nil, status.Errorf(codes.Internal, "convert to checkedExpr error: %s", err)
 		}
 
-		s.logger.Info().Msgf("checkedExpr: %#v", checkedExpr)
-
-		c := &converter{
-			logger: s.logger,
-		}
+		c := &converter{}
 		f, err := c.visit(checkedExpr.Expr)
 		if err != nil {
 			return nil, status.Errorf(codes.Internal, "convert to internal filter error: %s", err)
@@ -124,13 +110,9 @@ func (s *bookstoreService) ListBooks(ctx context.Context, in *pb.ListBooksReques
 	}
 
 	output := s.data
-	s.logger.Info().Str("output", fmt.Sprintf("%#v", output)).Msg("unfiltered output")
-
 	if internalFilter != nil {
 		output = internalFilter(output)
 	}
-
-	s.logger.Info().Str("output", fmt.Sprintf("%#v", output)).Msg("filtered output")
 
 	// FIXME handle pagination
 	return &pb.ListBooksResponse{Books: output}, nil
@@ -140,9 +122,7 @@ func (s *bookstoreService) ListBooks(ctx context.Context, in *pb.ListBooksReques
 //
 // WIP, inspired from cel2sql:
 // https://github.com/cockscomb/cel2sql/blob/c803dfd5c44ce5c5865ea98999164f5d54985d39/cel2sql.go#L33
-type converter struct {
-	logger zerolog.Logger
-}
+type converter struct{}
 
 func (c *converter) visit(expr *exprpb.Expr) (func(*pb.Book) bool, error) {
 	switch t := expr.ExprKind.(type) {
@@ -169,19 +149,18 @@ func (c *converter) visitCall(expr *exprpb.Expr) (func(*pb.Book) bool, error) {
 	// binary operators
 	case //operators.Add,
 		//operators.Divide,
-		//operators.Equals,
-		//operators.Greater,
-		//operators.GreaterEquals,
+		operators.Equals,
+		operators.Greater,
+		operators.GreaterEquals,
 		//operators.In,
-		//operators.Less,
-		//operators.LessEquals,
+		operators.Less,
+		operators.LessEquals,
 		//operators.LogicalAnd,
 		//operators.LogicalOr,
 		//operators.Multiply,
-		//operators.NotEquals,
+		operators.NotEquals:
 		//operators.OldIn,
 		//operators.Subtract
-		operators.LessEquals:
 		return c.visitCallBinary(expr)
 	// standard function calls.
 	default:
@@ -198,22 +177,44 @@ func (c *converter) visitCallBinary(expr *exprpb.Expr) (func(*pb.Book) bool, err
 	lhs := args[0]
 	rhs := args[1]
 
-	c.logger.Info().
-		Str("fun", fun).
-		Str("lhs", fmt.Sprintf("%#v", lhs.ExprKind)).
-		Str("rhs", fmt.Sprintf("%#v", rhs)).
-		Msg("visitCallBinary")
-
 	// FIXME this is not generic at all, implement the rest
-	if _, ok := lhs.ExprKind.(*exprpb.Expr_SelectExpr); ok {
-		if lhs.GetSelectExpr().GetOperand().GetIdentExpr().Name == "book" && lhs.GetSelectExpr().GetField() == "page_count" {
-			return func(book *pb.Book) bool {
-				condLHS := int64(book.GetPageCount())
-				condRHS := rhs.GetConstExpr().GetInt64Value()
-				return condLHS <= condRHS
-			}, nil
-		}
+
+	if _, ok := lhs.ExprKind.(*exprpb.Expr_SelectExpr); !ok {
+		return nil, fmt.Errorf("not implemented unless lhs is a SelectExpr")
 	}
 
-	return nil, fmt.Errorf("not implemented")
+	if lhs.GetSelectExpr().GetOperand().GetIdentExpr().Name != "book" {
+		return nil, fmt.Errorf("not implemented unless lhs is a SelectExpr on book")
+	}
+
+	if lhs.GetSelectExpr().GetField() != "page_count" {
+		return nil, fmt.Errorf("not implemented unless lhs is a SelectExpr on book.page_count")
+	}
+
+	ops := map[string]func(a, b int64) bool{
+		operators.Equals:        func(a, b int64) bool { return a == b },
+		operators.Greater:       func(a, b int64) bool { return a > b },
+		operators.GreaterEquals: func(a, b int64) bool { return a >= b },
+		operators.Less:          func(a, b int64) bool { return a < b },
+		operators.LessEquals:    func(a, b int64) bool { return a <= b },
+		operators.NotEquals:     func(a, b int64) bool { return a != b },
+	}
+
+	op, ok := ops[fun]
+	if !ok {
+		return nil, fmt.Errorf("not implemented unless func in in the supported ops")
+	}
+
+	if _, ok := rhs.ExprKind.(*exprpb.Expr_ConstExpr); !ok {
+		return nil, fmt.Errorf("not implemented unless rhs is a ConstExpr")
+	}
+	if _, ok := rhs.GetConstExpr().ConstantKind.(*exprpb.Constant_Int64Value); !ok {
+		return nil, fmt.Errorf("not implemented unless rhs is a ConstExpr.Int64Value")
+	}
+
+	return func(book *pb.Book) bool {
+		condLHS := int64(book.GetPageCount())
+		condRHS := rhs.GetConstExpr().GetInt64Value()
+		return op(condLHS, condRHS)
+	}, nil
 }
