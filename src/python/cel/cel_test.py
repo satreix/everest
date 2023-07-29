38ae96b7eebf88d3ed1406e83452d3b17cd8019c
satreix <satreix@gmail.com>
2023-07-29 23:42:10 +0100

    format(python): Use black

--- src/python/cel/cel_test.py
+++ src/python/cel/cel_test.py
@@ -6,16 +6,22 @@ import celpy
 class CelTest(unittest.TestCase):
     def test_cel_101(self):
         env = celpy.Environment()
-        ast = env.compile("""
+        ast = env.compile(
+            """
 account.balance >= transaction.withdrawal
     || (account.overdraftProtection
     && account.overdraftLimit >= transaction.withdrawal - account.balance)
-""")
+"""
+        )
         program = env.program(ast)
-        result = program.evaluate({
-            "account": celpy.json_to_cel({"balance": 500, "overdraftProtection": False}),
-            "transaction": celpy.json_to_cel({"withdrawal": 600}),
-        })
+        result = program.evaluate(
+            {
+                "account": celpy.json_to_cel(
+                    {"balance": 500, "overdraftProtection": False}
+                ),
+                "transaction": celpy.json_to_cel({"withdrawal": 600}),
+            }
+        )
         self.assertEqual(result, celpy.celtypes.BoolType(False))
 
 
