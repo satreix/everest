b2ea2c7db0215b9d6fbe97ff090a3b3406271faa
satreix <satreix@gmail.com>
2024-01-02 14:36:33 +0100

    build: Migrate haskell to bzlmod

--- third_party/haskell/update.sh
+++ third_party/haskell/update.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -eufo pipefail
 
-bazel run @stackage-unpinned//:pin
+bazel run @@rules_haskell~0.18~stack_snapshot~stackage-unpinned//:pin
+mv stackage_snapshot.json third_party/haskell/stackage_snapshot.json
