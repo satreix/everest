a4d00a32b40ab7818a581f052cc42b2e8c17b94d
satreix <satreix@gmail.com>
2024-01-02 12:03:49 +0100

    build: Migrate python and pip to bzlmod

--- third_party/python/update.sh
+++ third_party/python/update.sh
@@ -5,9 +5,6 @@ pipenv install --dev
 pipenv clean
 pipenv lock
 
-PY_BZL_FILE="third_party/python/requirements.bzl"
-bazel run //third_party/python:pipefile_lock_to_bzl -- \
-    --namespace=pypi \
-    --output="$PY_BZL_FILE" \
-    Pipfile.lock
-bazel run //:buildifier -- --lint=fix --warnings-all "$PY_BZL_FILE"
+pipenv requirements --dev > third_party/python/requirements.txt
+pipenv run pip-compile --generate-hashes --output-file=third_party/python/requirements_lock.txt third_party/python/requirements.txt
+rm third_party/python/requirements.txt
