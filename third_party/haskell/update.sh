1d817265622e8b8f4c0d68f44a58017df9d67c43
satreix <satreix@gmail.com>
2024-05-06 20:55:46 +0100

    deps: Fix haskell update script

--- third_party/haskell/update.sh
+++ third_party/haskell/update.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -eufo pipefail
 
-bazel run @@rules_haskell~0.18~stack_snapshot~stackage-unpinned//:pin
-mv stackage_snapshot.json third_party/haskell/stackage_snapshot.json
+bazel run @@rules_haskell~~stack_snapshot~stackage-unpinned//:pin
