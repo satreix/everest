f288e01877a9e83b5234fce47a74033a99d27541
satreix <satreix@gmail.com>
2024-06-16 18:43:39 +0100

    python: Migrate to poetry

--- third_party/python/update.sh
+++ third_party/python/update.sh
@@ -1,17 +1,4 @@
 #!/bin/bash
 set -eufo pipefail
 
-bazel build tools/pipenv
-PIPENV="$(bazel cquery --output=files tools/pipenv | grep -v '\.py$')"
-
-$PIPENV install --dev
-$PIPENV clean
-$PIPENV lock
-
-$PIPENV requirements --dev >third_party/python/requirements.txt
-$PIPENV run pip-compile \
-    --allow-unsafe \
-    --generate-hashes \
-    --output-file=third_party/python/requirements_lock.txt \
-    third_party/python/requirements.txt
-rm third_party/python/requirements.txt
+poetry export > third_party/python/requirements_lock.txt
