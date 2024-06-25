f118cc44a2ff4b6a5f5fedbe1e57fe7503977158
satreix <satreix@gmail.com>
2024-06-25 21:21:56 +0100

    feat: add java CEL example

---
+++ src/java/cel/CelTest.java
@@ -0,0 +1,42 @@
+package cel;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import dev.cel.common.CelAbstractSyntaxTree;
+import dev.cel.common.CelValidationException;
+import dev.cel.common.types.SimpleType;
+import dev.cel.compiler.CelCompiler;
+import dev.cel.compiler.CelCompilerFactory;
+import dev.cel.runtime.CelEvaluationException;
+import dev.cel.runtime.CelRuntime;
+import dev.cel.runtime.CelRuntimeFactory;
+import java.util.Map;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class CelTest {
+
+    private static CelCompiler celCompiler;
+    private static CelRuntime celRuntime;
+
+    @BeforeAll
+    public static void setUp() {
+        celCompiler = CelCompilerFactory.standardCelCompilerBuilder()
+            .addVar("my_var", SimpleType.STRING)
+            .build();
+        celRuntime = CelRuntimeFactory.standardCelRuntimeBuilder().build();
+    }
+
+    @Test
+    public void testCelCompilationAndEvaluation()
+        throws CelValidationException, CelEvaluationException {
+        CelAbstractSyntaxTree ast = celCompiler
+            .compile("my_var + '!'")
+            .getAst();
+        CelRuntime.Program program = celRuntime.createProgram(ast);
+        String result = (String) program.eval(Map.of("my_var", "Hello World"));
+
+        // Assert that the result is as expected
+        assertEquals("Hello World!", result);
+    }
+}
