3aa9ae4bdd4c85c6c79c44d9e337ca1abd19f812
satreix <satreix@gmail.com>
2024-09-04 16:50:08 +0100

    build(deps): udapte rules_oci to 2.0.0-rc0:x

--- src/java/cel/Cel.java
+++ src/java/cel/Cel.java
@@ -13,17 +13,14 @@ import java.util.Map;
 class Cel {
 
     private static final CelCompiler CEL_COMPILER =
-        CelCompilerFactory.standardCelCompilerBuilder()
-            .addVar("my_var", SimpleType.STRING)
-            .build();
+            CelCompilerFactory.standardCelCompilerBuilder()
+                    .addVar("my_var", SimpleType.STRING)
+                    .build();
     private static final CelRuntime CEL_RUNTIME =
-        CelRuntimeFactory.standardCelRuntimeBuilder().build();
+            CelRuntimeFactory.standardCelRuntimeBuilder().build();
 
-    public static void main(String[] args)
-        throws CelValidationException, CelEvaluationException {
-        CelAbstractSyntaxTree ast = CEL_COMPILER.compile(
-            "my_var + '!'"
-        ).getAst();
+    public static void main(String[] args) throws CelValidationException, CelEvaluationException {
+        CelAbstractSyntaxTree ast = CEL_COMPILER.compile("my_var + '!'").getAst();
         CelRuntime.Program program = CEL_RUNTIME.createProgram(ast);
         String result = (String) program.eval(Map.of("my_var", "Hello World"));
         System.out.println(result);
