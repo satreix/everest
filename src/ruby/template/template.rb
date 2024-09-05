84c7063d2e40febb3d48460aeebf8077d51b3e15
satreix <satreix@gmail.com>
2024-09-05 17:48:23 +0100

    update

--- src/ruby/template/template.rb
+++ src/ruby/template/template.rb
@@ -5,11 +5,10 @@ require 'haml'
 require 'slim'
 
 puts '=== ERB ==='
-template = ERB.new(File.read('src/ruby/template/templates/index.html.erb'))
-puts template.result_with_hash(name: 'ERB')
+puts ERB.new(File.read('src/ruby/template/templates/index.html.erb')).result_with_hash(name: 'ERB')
 
 puts '=== Haml ==='
-puts Haml::Engine.new(File.read('src/ruby/template/templates/index.html.haml')).render(Object.new, name: 'Haml')
+puts Haml::Template.new('src/ruby/template/templates/index.html.haml').render(Object.new, name: 'Haml')
 
 puts '=== Slim ==='
 puts Slim::Template.new('src/ruby/template/templates/index.html.slim').render(Object.new, name: 'Slim')
